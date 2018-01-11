9bbbacaf18f70203b53d844fe421536e230e2b4e
AnoJokamp354 <35319419+AnoJokamp354@users.noreply.github.com>
2018-01-11 09:55:44 +0700

    Update Prisma.js

--- Prisma.js
+++ Prisma.js
@@ -297,6 +297,8 @@ Prism.languages.javascript = {
 	},
 	'keyword': /\b(var|let|if|else|while|do|for|return|in|instanceof|function|new|with|typeof|try|catch|finally|null|break|continue)\b/g,
 	'boolean': /\b(true|false)\b/g,
+	'android': /\b(android)\b/g,
+	'androperty': /\b(import)\b/g,
 	'number': /\b-?(0x)?\d*\.?\d+\b/g,
 	'operator': /[-+]{1,2}|!|=?&lt;|=?&gt;|={1,2}|(&amp;){1,2}|\|?\||\?|\*|\//g,
 	'ignore': /&(lt|gt|amp);/gi,

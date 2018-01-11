e5628234ba16c4649b8c73a8c3fdf6aa46f17671
AnoJokamp354 <35319419+AnoJokamp354@users.noreply.github.com>
2018-01-11 10:58:14 +0700

    Update Prisma.js

--- Prisma.js
+++ Prisma.js
@@ -286,7 +286,6 @@ if (Prism.languages.markup) {
 	});
 }
 Prism.languages.javascript = {
-	'commandline': /\b($ |#)\b/g,
 	'comment': {
 		pattern: /(^|[^\\])(\/\*[\w\W]*?\*\/|\/\/.*?(\r?\n|$))/g,
 		lookbehind: true
